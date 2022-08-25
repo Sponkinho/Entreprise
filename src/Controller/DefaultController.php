4066de2effc12192e5dfec4441b016dac0103de6
Sponkinho <swan.duval95@gmail.com>
2022-08-25 13:50:32 +0200

    create show employes action

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Employe;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default_home')]
-    public function home(): Response
-    {
-        return $this->render('default/home.html.twig');
+    public function home(EntityManagerInterface $entityManager): Response
+    {   //Permet de recuperer toutes les lignes de la table employe
+        $employes = $entityManager->getRepository(Employe::class)->findAll();
+        
+        //On passe le variable $employes a render Twig pour les afficher
+        return $this->render('default/home.html.twig', [
+            'employes' => $employes
+        ]);
     }
 }
