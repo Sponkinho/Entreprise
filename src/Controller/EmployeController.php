82b0f3dbad5253af0aa6c72c3150749eb85c5239
Sponkinho <swan.duval95@gmail.com>
2022-08-25 15:24:23 +0200

    Update action done

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -69,4 +69,24 @@ class EmployeController extends AbstractController
         ]);
     } // end function create
 
+    #[Route('/modifier-un-employe/{id}', name: 'update_employe', methods: ['GET', 'POST'])]
+    public function updateEmploye(Employe $employe, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EmployeFormType::class, $employe)
+            ->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $entityManager->persist($employe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('default_home');
+        }
+
+        return $this->render('form/employe.html.twig', [
+            'form_employe' => $form->createView(),
+            'employe' => $employe
+        ]);
+    }
+
 }// end class
